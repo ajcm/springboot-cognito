a5a5ce195b406777f7624640c87da74cc5db8cac
ajcm <ajcm2020@proton.me>
2025-05-22 13:23:04 +0100

    fix spring

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 #docker build . -t ajcm/springboot-cognito:latest
 #docker tag ajcm/springboot-cognito:latest 423623860374.dkr.ecr.eu-west-1.amazonaws.com/springboot-cognito:latest
 
-
-docker build . -t 423623860374.dkr.ecr.eu-west-1.amazonaws.com/springboot-cognito:latest
-docker push 423623860374.dkr.ecr.eu-west-1.amazonaws.com/springboot-cognito:latest
+docker build . -t 423623860374.dkr.ecr.eu-west-1.amazonaws.com/springboot-cognito:spring-cognito-nojws
+docker push 423623860374.dkr.ecr.eu-west-1.amazonaws.com/springboot-cognito:spring-cognito-nojws
